1e9a80eebe50fbf98126db00b580a05f06ca02b6
Francisco Diaz <sisco@Siscos-MacBook-Pro.local>
2018-07-23 00:30:21 -0700

    cant get jquery to work

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Sisco",
+        "photo": "",
+        "scores": [
+            3,
+            1,
+            4,
+            5,
+            2,
+            3,
+            1,
+            5,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Spider-Man",
+        "photo": "",
+        "scores": [
+            4,
+            5,
+            5,
+            4,
+            3,
+            4,
+            5,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Batman",
+        "photo": "",
+        "scores": [
+            3,
+            4,
+            2,
+            5,
+            4,
+            1,
+            1,
+            1,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Yoda",
+        "photo": "",
+        "scores": [
+            1,
+            1,
+            4,
+            5,
+            5,
+            2,
+            4,
+            4,
+            4,
+            4
+        ]
+    }
+];
+
+module.exports = friends;
